ceb0d478fd1f15fa7c7fef17af7375f752b68f6e
Evert <weball1@catamount.wcu.edu>
2019-09-04 12:51:01 -0400

    Added documentation and finished constructor

--- Person.java
+++ Person.java
@@ -1,8 +1,26 @@
+/**
+ * Represents a person in our Database.
+ *
+ * @author Evert Ball
+ * @version 09/18/2019
+ */
 public class Person {
-    
-    public Person() {
 
-        //TODO Finish constructor
+    /** First Name */
+    private String first;
+    /** Last name */
+    private String last;
+    /** Marital Status */
+    private Status status;
+
+    /**
+     * Constructs a person object
+     */
+    public Person(String firstname, String lastname, Status status) {
+
+        this.first  = firstname;
+        this.last   = lastname;
+        this.status = status;
 
     } // end Person constructor
 
--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# Database
+# Linked List Database
 
 ## Description
-
+Acts as a Database using Linked Lists as tables.
 ## Usage
+First, compile the source code:
 
+```bash
+javac *.java
+```
+
+To use, just run the Database by typing
+
+```bash
+java Database
+```
+and following the on screen prompts.
 ## Other
